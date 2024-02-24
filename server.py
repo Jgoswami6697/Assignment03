48fba60f03c6507fa30d271b4402a1422219908d
JAYGIRI GOSWAMI <rdj157003@gmail.com>
2024-02-24 18:21:52 -0500

    server started listening

--- server.py
+++ server.py
@@ -10,3 +10,15 @@ with open('config.json', 'r') as f:
 
 HOST = config['HOST']  # Standard loopback interface address (localhost)
 PORT = config['PORT']  # Port to listen on (non-privileged ports are > 1023)
+
+def log(message, level):
+    with open('log.txt', 'a') as f:
+        f.write(f'{datetime.now()} - {level} - {message}\n')
+
+# Create a TCP/IP socket
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    # Bind the socket to the port
+    s.bind((HOST, PORT))
+    # Listen for incoming connections
+    s.listen()
+    log('Server is listening', 'INFO')
