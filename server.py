03ad1c6f8d1a24d86d37b5022dbc2540e9771393
JAYGIRI GOSWAMI <rdj157003@gmail.com>
2024-02-24 17:57:43 -0500

    aloading the parameters with HOSt and PORT

--- server.py
+++ server.py
@@ -3,3 +3,7 @@ import json
 from datetime import datetime
 from collections import defaultdict
 import time
+
+# Load configuration from a JSON file
+with open('config.json', 'r') as f:
+    config = json.load(f)
