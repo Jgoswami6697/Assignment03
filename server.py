0ff2785392a2354aa46be44f2d990c906d3f319b
JAYGIRI GOSWAMI <rdj157003@gmail.com>
2024-02-24 18:53:06 -0500

    server started aceepting the logs and alo created to record the time for noisy client

--- server.py
+++ server.py
@@ -22,3 +22,20 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     # Listen for incoming connections
     s.listen()
     log('Server is listening', 'INFO')
+
+    while True:
+        # Wait for a connection
+        try:
+            conn, addr = s.accept()
+            with conn:
+                log(f'Connected by {addr}', 'INFO')
+                data = conn.recv(1024)
+                if not data:
+                    log(f'Client {addr} disconnected', 'INFO')
+                    break
+                message = data.decode('utf-8')
+
+                # Update the message count and start time for this client
+                message_counts[addr] += 1
+                if message_counts[addr] == 1:
+                    start_times[addr] = time.time()
