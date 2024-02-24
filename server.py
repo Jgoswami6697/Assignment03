fee2af00cd060e33ab7072f9173416ce8802addf
JAYGIRI GOSWAMI <rdj157003@gmail.com>
2024-02-24 18:08:50 -0500

    Importing the values from json

--- server.py
+++ server.py
@@ -7,3 +7,6 @@ import time
 # Load configuration from a JSON file
 with open('config.json', 'r') as f:
     config = json.load(f)
+
+HOST = config['HOST']  # Standard loopback interface address (localhost)
+PORT = config['PORT']  # Port to listen on (non-privileged ports are > 1023)
